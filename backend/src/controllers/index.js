b71997faf4575907368649992eb4b83fca54015e
João Pedro P <68718789+joaopedro29@users.noreply.github.com>
2021-04-19 00:31:40 -0300

    Gerando o token no cadastro do usuário

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,12 +1,22 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv');
 
+dotenv.config()
 
 // Método para criptografar a senha do usuário e retornar a senha modificada.
 exports.HashPassword = async (password) =>{
 
     const hash = await bcrypt.hash(password, 10)
-
     return hash
+
+}
+
+exports.getToken = async (id, name , email) =>{
+
+    const token = await jwt.sign({id , name, email} , process.env.TOKEN_USER);
+
+    return token
 
 }
  
--- backend/src/controllers/users/public.controllers.js
+++ backend/src/controllers/users/public.controllers.js
@@ -1,6 +1,6 @@
 
 const db = require('../../configs/database')
-const {HashPassword} = require('../index');
+const {HashPassword , getToken} = require('../index');
 
 exports.NewsREgister = async (req, res) =>{
 
@@ -8,7 +8,7 @@ exports.NewsREgister = async (req, res) =>{
 
     try {
 
-        // Converte date for pt-BR
+        //#region Converte date para pt-BR
 
         var today = new Date().toLocaleDateString('pt-BR')
 
@@ -16,15 +16,28 @@ exports.NewsREgister = async (req, res) =>{
 
         today = yyyy + '-' + mm + '-' + dd;
 
-        //
+        //#endregion
 
         let Hash = await HashPassword(password)
 
         await db.query(`INSERT INTO users(name , email , password , CPF , registration_date)
         values( $1 , $2 , $3 , $4 , $5)`,[name,email,Hash,CPF,today])
 
-        res.send({message:'cadastro efetuado com sucesso!'})
-        
+        //#region TOKEN
+
+        // selecionando o id do usuário no banco para adicionar no parametro do token
+        let {rows} = await db.query('SELECT id_user FROM users WHERE email=$1' , [email])
+
+        let token =  await getToken(rows[0].id_user , name , email) 
+        // Init JSON(tokens)
+        let tokens = { tokens:[{ token:token}]}
+        // Adicionando o token nos dados do usuário
+        await db.query(`UPDATE users SET tokens=$1 WHERE id_user=$2` , [tokens , rows[0].id_user])
+
+        //#endregion
+
+        res.send({message:'cadastro efetuado com sucesso' , token})
+
     } catch (error) {
 
         res.status(401).send({error:error})
